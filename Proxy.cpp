63c98700841166e4997f572c5708f7f921bc537b
MeriBaveyan <90473368+MeriBaveyan@users.noreply.github.com>
2022-12-12 17:41:46 +0400

    Create Proxy.cpp

---
+++ Proxy.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <memory>
+//#include <string>
+
+class RequestProjectIP;
+class Proxy;
+
+class IRequestIP
+{
+	virtual void RequestIP() const = 0;
+};
+
+class RequestProjectIP : public IRequestIP
+{
+public:
+
+	RequestProjectIP(std::string ID = "us01abc") : m_projectID(ID) {};
+
+	std::string GetProjectID() const
+	{
+		return m_projectID;
+	}
+
+	void RequestIP() const override {
+		std::cout << "Request from original IP.\n";
+	}
+
+private:
+	std::string m_projectID;
+};
+
+class Proxy : public IRequestIP
+{
+public:
+	Proxy(std::unique_ptr<RequestProjectIP> pRequestIP = std::make_unique<RequestProjectIP>())
+	{
+		m_pRequestIP = std::move(pRequestIP);
+	}
+
+	void RequestIP() const override {
+		std::cout << "Request from Proxy.\n";
+	}
+
+private:
+	std::unique_ptr<RequestProjectIP> m_pRequestIP;
+
+	bool CheckProjectID(const std::string projectID)
+	{
+		if (!(projectID.compare(m_pRequestIP->GetProjectID())))
+		{
+			RequestIP();
+		}
+	}
+};
+
+int main()
+{
+	std::unique_ptr<RequestProjectIP> pRequestIP_real = std::make_unique<RequestProjectIP>();
+	std::unique_ptr<Proxy> pRequestIP_proxy(std::unique_ptr<RequestProjectIP>);
+	pRequestIP_real->RequestIP();
+	//pRequestIP_proxy->RequestIP();
+}
