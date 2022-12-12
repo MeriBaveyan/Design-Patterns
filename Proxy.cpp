72d4afea9ccaf9e9bd66c7fb0c5cb415583667d3
MeriBaveyan <90473368+MeriBaveyan@users.noreply.github.com>
2022-12-12 17:52:25 +0400

    Update Proxy.cpp

--- Proxy.cpp
+++ Proxy.cpp
@@ -1,3 +1,66 @@
+//#include <iostream>
+//#include <memory>
+////#include <string>
+//
+//class RequestProjectIP;
+//class Proxy;
+//
+//class IRequestIP
+//{
+//	virtual void RequestIP() const = 0;
+//};
+//
+//class RequestProjectIP: public IRequestIP
+//{
+//public:
+//
+//	RequestProjectIP(std::string ID = "us01abc") : m_projectID(ID) {};
+//
+//	std::string GetProjectID() const
+//	{
+//		return m_projectID;
+//	}
+//
+//	void RequestIP() const override {
+//		std::cout << "Request from original IP.\n";
+//	}
+//
+//private:
+//	std::string m_projectID;
+//};
+//
+//class Proxy : public IRequestIP
+//{
+//public:
+//	Proxy(std::unique_ptr<RequestProjectIP> pRequestIP = std::make_unique<RequestProjectIP>())
+//	{
+//		m_pRequestIP = std::move(pRequestIP);
+//	}
+//
+//	void RequestIP() const override {
+//		std::cout << "Request from Proxy.\n";
+//	}
+//
+//private:
+//	std::unique_ptr<RequestProjectIP> m_pRequestIP;
+//
+//	bool CheckProjectID(const std::string projectID)
+//	{
+//		if (!(projectID.compare(m_pRequestIP->GetProjectID())))
+//		{
+//			RequestIP();
+//		}
+//	}
+//};
+//
+//int main()
+//{
+//	std::unique_ptr<RequestProjectIP> pRequestIP_real = std::make_unique<RequestProjectIP>();
+//	std::unique_ptr<Proxy> pRequestIP_proxy;
+//	pRequestIP_real->RequestIP();
+//	pRequestIP_proxy->RequestIP();
+//}
+
 #include <iostream>
 #include <memory>
 //#include <string>
@@ -56,7 +119,9 @@ private:
 int main()
 {
 	std::unique_ptr<RequestProjectIP> pRequestIP_real = std::make_unique<RequestProjectIP>();
-	std::unique_ptr<Proxy> pRequestIP_proxy(std::unique_ptr<RequestProjectIP>);
+	//std::unique_ptr<Proxy> pRequestIP_proxy = std::make_unique<Proxy>(new Proxy());
+	std::unique_ptr<Proxy> pRequestIP_proxy = std::make_unique<Proxy>(std::unique_ptr<RequestProjectIP>());
+	//std::unique_ptr<Proxy> pRequestIP_proxy(std::unique_ptr<RequestProjectIP>);
 	pRequestIP_real->RequestIP();
-	//pRequestIP_proxy->RequestIP();
+	pRequestIP_proxy->RequestIP();
 }
